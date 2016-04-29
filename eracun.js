8cc65e7ffca51b077e7094e45cc9e379158b2d15
Eragon peerGynt <eragon.peergynt@gmail.com>
2016-04-29 14:55:49 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -194,7 +194,7 @@ var vrniRacune = function(callback) {
     }
   );
 }
-
+var textReturner = "";
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
@@ -209,12 +209,19 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
+      stmt.finalize();//stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
+    if(napaka1 || napaka2) {
+        textReturner = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+        odgovor.redirect("/prijava"); 
+      }
+      else {
+        textReturner = "Stranka je bila uspešno registrirana.";
+        odgovor.redirect("/prijava"); 
+      }
     odgovor.end();
   });
 })
@@ -223,7 +230,8 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        //odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: textReturner, seznamStrank: stranke, seznamRacunov: racuni});  
       }) 
     });
 })
